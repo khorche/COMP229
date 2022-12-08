0f3053cd482988ee6a86b43f8d849df06fb367e0
Khorche <khorchepakoda@gmail.com>
2022-12-08 13:50:37 +0530

    Added Frontend Views

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs');
+
 mongoose.connect('mongodb://localhost:27017/survey_site', {useNewUrlParser: true, useUnifiedTopology: true});
 
 require('./routes')(app);
